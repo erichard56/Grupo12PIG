22aee6974a7b1713e109c813bb9046258fa39d78
erichard56 <enrique.richard@gmail.com>
2023-04-12 14:16:14 -0300

    PIG -- commit inicial

---
+++ contacto/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from .forms import FormularioContacto
+
+# Create your views here.
+
+def contacto(request):
+    formulario_contacto = FormularioContacto()
+
+    if (request.method == 'POST'):
+        formulario_contacto = FormularioContacto(data=request.POST)
+        if (formulario_contacto.is_valid()):
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+
+            email = EmailMessage('Mensaje desde App Django', 
+                'El usuario con nombre {} con la direccion {} escribe lo siguiente: \n\n{}'.format(nombre, email, contenido),
+                '', ['enrique.richard@gmail.com'], 
+                reply_to=[email])
+            try:
+                email.send()
+                return redirect('/contacto/?valido')
+            except:
+                return redirect('/contacto/?novalid')
+
+    return render(request, 'contacto/contacto.html', {"miFormulario":formulario_contacto})
+    
