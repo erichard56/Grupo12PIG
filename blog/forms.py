ac3e908be0b4760a1675fc8146fe50d11427d88e
erichard56 <enrique.richard@gmail.com>
2023-04-17 15:12:25 -0300

    fix labels

--- blog/forms.py
+++ blog/forms.py
@@ -4,9 +4,9 @@ from .models import Categoria
 
 class NewblogForm(forms.Form):
     titulo = forms.CharField(label='Titulo', max_length=50)
-    contenido = forms.CharField(label='contenido', max_length=50)
-    imagen = forms.ImageField(label='imagen', max_length=50)
-    autor = forms.ModelChoiceField(label='autor', queryset=User.objects.all())
+    contenido = forms.CharField(label='Contenido', max_length=50)
+    imagen = forms.ImageField(label='Imagen', max_length=50)
+    autor = forms.ModelChoiceField(label='Autor', queryset=User.objects.all())
     categorias = forms.ModelChoiceField(label='Categoria', queryset=Categoria.objects.all())
 
 #color = forms.ModelChoiceField(queryset=Color.objects.all())
