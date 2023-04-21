741253f53ec882c1963e357bd0301dc9660db81f
erichard56 <enrique.richard@gmail.com>
2023-04-21 18:04:38 -0300

    llevo contenido a  500 chars y fix campo updated

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Categoria(models.Model):
 	nombre = models.CharField(max_length=50)
 	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
 
 	class Meta:
 		verbose_name = 'categoria'
@@ -17,12 +17,12 @@ class Categoria(models.Model):
 
 class Post(models.Model):
 	titulo = models.CharField(max_length=50)
-	contenido = models.CharField(max_length=50)
+	contenido = models.TextField(max_length=500)
 	imagen = models.ImageField(upload_to='blog', null=True, blank=True)
 	autor = models.ForeignKey(User, on_delete=models.CASCADE)
 	categorias = models.ManyToManyField(Categoria)
 	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
 
 	class Meta:
 		verbose_name = 'post'
