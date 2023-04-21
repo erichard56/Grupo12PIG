9e7af34648ff5edf413b77e5949cea32e9c612a3
erichard56 <enrique.richard@gmail.com>
2023-04-21 17:10:08 -0300

    agregado static_root en settings.py

--- pig/settings.py
+++ pig/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 STATICFILES_DIRS= [ BASE_DIR / 'static', ]
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static_root'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
