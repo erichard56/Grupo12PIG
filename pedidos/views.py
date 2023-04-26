819e36ea71906149ba2930f1a70ef215b0edde6d
erichard56 <enrique.richard@gmail.com>
2023-04-26 09:09:56 -0300

    limpiar carro luego de confirmar envio

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from pedidos.models import LineaPedido, Pedidos
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
 from carro.carro import Carro
+from django.contrib.auth.models import User
+from pedidos.models import LineaPedido, Pedidos
+from tienda.models import Producto
+from datetime import datetime
 
 # Create your views here.
 
 @login_required(login_url='/autenticacion/logear')
 def procesar_pedido(request):
+	usuario = User.objects.all().filter(username=request.user)
 	pedido = Pedidos.objects.create(user=request.user)
 	carro = Carro(request)
 	lineas_pedido = list()
 	for key, value in carro.carro.items():
 		lineas_pedido.append([key, value])
+		producto = Producto.objects.all().filter(id = value['producto_id'])
+		lp = LineaPedido()
+		lp.user = usuario[0]
+		lp.producto = producto[0]
+		lp.pedido = pedido
+		lp.cantidad = value['cantidad']
+		lp.created_at = datetime.now()
+		linea_pedido = lp.save()
 
 	enviar_mail(pedido = pedido, 
 		lineas_pedido = lineas_pedido, 
 		nombreusuario = request.user.username,
 		emailusuario = request.user.email)
 
+	carro.limpiar_carro()
 	messages.success(request, 'EL pedido se ha creado correctamente')
 	return redirect('../tienda')
 
