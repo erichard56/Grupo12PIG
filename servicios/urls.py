f3a51653a8e8eec743fd90906f4e2f429d7eb3cf
erichard56 <enrique.richard@gmail.com>
2023-05-16 15:34:47 -0300

    Agregado Borrado a Servicios

--- servicios/urls.py
+++ servicios/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.servicios, name='Servicios'),
+    path('servicio_borrar/<int:id>', views.servicio_borrar, name='servicio_borrar'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- servicios/views.py
+++ servicios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from servicios.models import Servicio
 
 # Create your views here.
@@ -6,4 +6,10 @@ def servicios(request):
     servicios = Servicio.objects.all().order_by('-updated')
     return render(request, 'servicios/servicios.html', {"servicios":servicios})
 
-
+def servicio_borrar(request, id):
+    try:
+        servicio = Servicio.objects.get(id = id)
+    except:
+        return redirect('/servicios')
+    servicio.delete()
+    return redirect('/servicios')
